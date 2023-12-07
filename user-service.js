026f489d68b2af5018da4b926175f9262359a088
NicoleAlexBarrero <113063376+NicoleAlexBarrero@users.noreply.github.com>
2023-12-06 20:26:54 -0500

    Add files via upload

--- user-service.js
+++ user-service.js
@@ -32,7 +32,7 @@ module.exports.connect = function () {
     });
 };
 
-module.exports.registerUser = function (userData) {
+module.exports.registerUser = function (userData) {``
     return new Promise(function (resolve, reject) {
 
         if (userData.password != userData.password2) {
